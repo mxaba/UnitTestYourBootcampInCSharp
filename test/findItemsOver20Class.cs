682f5ec27773a7637a129fac114fd1f8dc4bc8ad
mxaba <samuel.mcebo@gmail.com>
2021-07-30 17:59:23 +0200

    teste added

---
+++ test/findItemsOver20Class.cs
@@ -0,0 +1,12 @@
+using System;
+using Xunit;
+
+namespace Functions.test {
+    public class FindItemsOver20
+    {
+        [Fact]
+        public void findItemsOver20() {
+            var findItemsOver20 = new FindItemsOver20Class();
+        }
+    }
+}
