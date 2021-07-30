cef0ba050809d9ee68984f53e85d91731e3b59e9
mxaba <samuel.mcebo@gmail.com>
2021-07-30 11:22:06 +0200

    Is

---
+++ src/Functions/isFromBellville.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Functions {
+    public class FromBellvilleClass {
+        public bool IsFromBellville(string name) {
+            return name.StartsWith("CY");
+        }
+    }
+}
