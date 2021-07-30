21fdb692ffee562139fce918f41553ba33408dcf
mxaba <samuel.mcebo@gmail.com>
2021-07-30 16:09:40 +0200

    itemsOver

---
+++ src/Functions/findItemsOver.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+using System;
+
+namespace Functions {
+    public class FindItemsOverClass {
+
+        public class Items {
+            public int Qty {get; set;}
+            public string Name {get; set;}
+        }
+
+        public List<Items> FindItemsOver(List<Items> listOfObjects, int qty) {
+            var newListOfObjects = new List<Items>();
+            foreach (var item in listOfObjects) {
+                if (item.Qty > qty) {
+                    newListOfObjects.Add(item);
+                    Console.WriteLine($"Inside if: {item.Name}");
+                }
+            }
+            return newListOfObjects;
+        }
+    }
+}
