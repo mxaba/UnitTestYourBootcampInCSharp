10d4029f0fc276d69a3eb4616f3977c42997b0ef
mxaba <samuel.mcebo@gmail.com>
2021-07-30 11:22:49 +0200

    T

---
+++ src/Functions/totalPhoneBill.cs
@@ -0,0 +1,20 @@
+namespace Functions {
+    public class TotalPhoneBillClass {
+        public string TotalPhoneBill(string name){
+            var nameSplited = name.Split(", ");
+            double call = 0;
+            double sms = 0;
+            for (var index = 0; index<nameSplited.Length; index++){
+                var trim = nameSplited[index].Trim();
+                trim = trim.ToLower();
+                if (trim == "call"){
+                    call++;
+                } if (trim == "sms"){
+                    sms++;
+                }
+            }
+            double totalCost = (double)(2.75 * call + 0.65 * sms);
+            return $"R{totalCost:N2}";
+        }
+    }
+}
