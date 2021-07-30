c72fd98e8be9dd60eeb3c1d2e768776409f1c778
mxaba <samuel.mcebo@gmail.com>
2021-07-30 11:21:24 +0200

    from

---
+++ src/Functions/fromWhere.cs
@@ -0,0 +1,16 @@
+namespace Functions {
+    public class FromWhereClass {
+        public string FromWhere(string city) {
+            switch (city.ToLower()) {
+                case "cy":
+                    return "Bellville";
+                case "cj":
+                    return "Paarl";
+                case "ca":
+                    return "Cape Town";
+                default:
+                    return "Some other place!";
+            }
+        }
+    }
+}
