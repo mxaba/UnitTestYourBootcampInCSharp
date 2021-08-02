25138668cd055f8c8bee287760a8ab4a6d992a06
mxaba <samuel.mcebo@gmail.com>
2021-08-02 12:55:53 +0200

    using Newtonsoft json

--- test/findItemsOver.cs
+++ test/findItemsOver.cs
@@ -2,13 +2,14 @@ using System;
 using Xunit;
 using System.Collections.Generic;
 using static BootcampFunctions.FindItemsOverClass;
+using Newtonsoft.Json;
 
 namespace BootcampFunctions.test {
     public class FindItemsOver
     {
 
         [Fact]
-        public void findItemsOver() {
+        public void findItemsOver45() {
             var findItemsOver = new FindItemsOverClass();
 
             var itemsListOne =  new List<Items>() { 
@@ -21,10 +22,64 @@ namespace BootcampFunctions.test {
             var itemsListTwo =  new List<Items>() { 
                 new Items(){Name="Banna", Qty = 56},
             };
+
             var overs = findItemsOver.FindItemsOver(itemsListOne, 45);
+
+            var obj1Str = JsonConvert.SerializeObject(overs);
+            var obj2Str = JsonConvert.SerializeObject(itemsListTwo);
+            Assert.Equal(obj1Str, obj2Str );
             Assert.Equal(itemsListTwo.ToArray().Length, findItemsOver.FindItemsOver(itemsListOne, 45).ToArray().Length);
-            // Assert.IsTrue(itemsListTwo.SequenceEqual(overs));
-            // itemsListTwo.ShouldDeepEqual(findItemsOver20.FindItemsOver20(itemsListOne));
+        }
+
+        [Fact]
+        public void findItemsOver10() {
+            var findItemsOver = new FindItemsOverClass();
+
+            var itemsListOne =  new List<Items>() { 
+                new Items(){ Qty = 56, Name="Banna"},
+                new Items(){ Qty = 43, Name="Apple"},
+                new Items(){ Qty = 2, Name="Rama"},
+                new Items(){ Qty = 14, Name="Bread"}
+            };
+
+            var itemsListTwo =  new List<Items>() { 
+                new Items(){ Qty = 56, Name="Banna"},
+                new Items(){ Qty = 43, Name="Apple"},
+                new Items(){ Qty = 14, Name="Bread"}
+            };
+
+            var overs = findItemsOver.FindItemsOver(itemsListOne, 10);
+
+            var obj1Str = JsonConvert.SerializeObject(overs);
+            var obj2Str = JsonConvert.SerializeObject(itemsListTwo);
+            Assert.Equal(obj1Str, obj2Str );
+            Assert.Equal(itemsListTwo.ToArray().Length, overs.ToArray().Length);
+        }
+
+        [Fact]
+        public void findItemsOver2() {
+            var findItemsOver = new FindItemsOverClass();
+
+            var itemsListOne =  new List<Items>() { 
+                new Items(){ Qty = 56, Name="Banna"},
+                new Items(){ Qty = 43, Name="Apple"},
+                new Items(){ Qty = 4, Name="Rama"},
+                new Items(){ Qty = 14, Name="Bread"}
+            };
+
+            var itemsListTwo =  new List<Items>() {
+                new Items(){ Qty = 56, Name="Banna"},
+                new Items(){ Qty = 43, Name="Apple"},
+                new Items(){ Qty = 4, Name="Rama"},
+                new Items(){ Qty = 14, Name="Bread"}
+            };
+
+            var overs = findItemsOver.FindItemsOver(itemsListOne, 2);
+
+            var obj1Str = JsonConvert.SerializeObject(overs);
+            var obj2Str = JsonConvert.SerializeObject(itemsListTwo);
+            Assert.Equal(obj1Str, obj2Str );
+            Assert.Equal(itemsListTwo.ToArray().Length, overs.ToArray().Length);
         }
     }
 }
--- test/findItemsOver20.cs
+++ test/findItemsOver20.cs
@@ -2,12 +2,13 @@ using System;
 using Xunit;
 using System.Collections.Generic;
 using static BootcampFunctions.FindItemsOver20Class;
+using Newtonsoft.Json;
 
 namespace BootcampFunctions.test {
     public class FindItemsOver20
     {
         [Fact]
-        public void findItemsOver20() {
+        public void findItemsOver20ShouldReturn2Items() {
             var findItemsOver20 = new FindItemsOver20Class();
 
             var itemsListOne =  new List<Items>() { 
@@ -22,9 +23,42 @@ namespace BootcampFunctions.test {
                 new Items(){ Qty = 43, Name="Apple"},
             };
 
-            // var overs = findItemsOver20.FindItemsOver20(itemsListOne);
-            Assert.Equal(itemsListTwo.ToArray().Length, findItemsOver20.FindItemsOver20(itemsListOne).ToArray().Length);
+            var overs = findItemsOver20.FindItemsOver20(itemsListOne);
+
+            var obj1Str = JsonConvert.SerializeObject(overs);
+            var obj2Str = JsonConvert.SerializeObject(itemsListTwo);
+            Assert.Equal(obj1Str, obj2Str );
+
+            Assert.Equal(itemsListTwo.ToArray().Length, overs.ToArray().Length);
             // itemsListTwo.ShouldDeepEqual(findItemsOver20.FindItemsOver20(itemsListOne));
         }
+
+        [Fact]
+        public void findItemsOver20ShouldReturn3Items() {
+            var findItemsOver20 = new FindItemsOver20Class();
+
+            var itemsListOne =  new List<Items>() { 
+                new Items(){ Qty = 56, Name="Banna"},
+                new Items(){ Qty = 43, Name="Apple"},
+                new Items(){ Qty = 2, Name="Rama"},
+                new Items(){ Qty = 41, Name="Bread"}
+            };
+
+            var itemsListTwo =  new List<Items>() { 
+                new Items(){ Qty = 56, Name="Banna"},
+                new Items(){ Qty = 43, Name="Apple"},
+                new Items(){ Qty = 41, Name="Bread"}
+            };
+
+            var overs = findItemsOver20.FindItemsOver20(itemsListOne);
+
+            var obj1Str = JsonConvert.SerializeObject(overs);
+            var obj2Str = JsonConvert.SerializeObject(itemsListTwo);
+            Assert.Equal(obj1Str, obj2Str );
+
+            Assert.Equal(itemsListTwo.ToArray().Length, overs.ToArray().Length);
+            // itemsListTwo.ShouldDeepEqual(findItemsOver20.FindItemsOver20(itemsListOne));
+        }
+
     }
 }
