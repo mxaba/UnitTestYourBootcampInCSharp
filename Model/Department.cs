e92081c348b841527de1ccd6a190e258e06ac8ff
mxaba <samuel.mcebo@gmail.com>
2021-08-03 14:30:13 +0200

    fixed and rearange my files, i have a model now

---
+++ Model/Department.cs
@@ -0,0 +1,7 @@
+namespace Model {
+    public class Department {
+        public int Sales {get; set;}
+        public string DepartmentName {get; set;}
+        public string Day {get; set;}
+    }
+}
---
+++ Model/Item.cs
@@ -0,0 +1,6 @@
+namespace Model {
+    public class Item {
+        public int Qty {get; set;}
+        public string Name {get; set;}
+    }
+}
