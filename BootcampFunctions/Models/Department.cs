5a3c7d821dddd54b38521e8aef427c3cf0f3bc05
mxaba <samuel.mcebo@gmail.com>
2021-08-04 07:37:10 +0200

    fixed the model

---
+++ BootcampFunctions/Models/Department.cs
@@ -0,0 +1,7 @@
+namespace BootcampFunctions.Models {
+    public class Department {
+        public int Sales {get; set;}
+        public string DepartmentName {get; set;}
+        public string Day {get; set;}
+    }
+}
---
+++ BootcampFunctions/Models/Item.cs
@@ -0,0 +1,6 @@
+namespace BootcampFunctions.Models {
+    public class Item {
+        public int Qty {get; set;}
+        public string Name {get; set;}
+    }
+}
