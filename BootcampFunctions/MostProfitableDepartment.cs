6ac9da540904c5dee68eede6fd9d02b02dc48569
mxaba <samuel.mcebo@gmail.com>
2021-08-11 22:18:05 +0200

    fixed mostprofitableprice

--- BootcampFunctions/MostProfitableDepartment.cs
+++ BootcampFunctions/MostProfitableDepartment.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using BootcampFunctions.Models;
 
@@ -8,16 +9,27 @@ namespace BootcampFunctions {
         public string mostProfitableDepartment(List<Department> listOfObjects) {
             var mostDepartment = "";
             var totalSales = 0;
+            var myDict = new Dictionary<string, int>();
 
             for(var i = 0; i<listOfObjects.Count; i++){
                 var sales = listOfObjects[i].Sales;
                 var department = listOfObjects[i].DepartmentName;
+                
+                if(!myDict.ContainsKey(department)){
+                    myDict.Add(department, 0);
+                }
+                myDict[department] += sales;
+            }
 
-                if (sales > totalSales) {
-                    totalSales = sales;
-                    mostDepartment = department;
+            foreach(KeyValuePair<string, int> item in myDict)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value}");
+                if(item.Value > totalSales){
+                    totalSales = item.Value;
+                    mostDepartment = item.Key;
                 }
             }
+            
             return mostDepartment;
         }
     }
